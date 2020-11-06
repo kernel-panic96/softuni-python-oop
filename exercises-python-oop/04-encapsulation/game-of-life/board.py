8021f4ae15eec6d4c9447afcb5f9a894f011d5ec
yrl <yavor@uber.com>
2020-11-06 21:53:52 +0200

    Implement console game of life

---
+++ exercises-python-oop/04-encapsulation/game-of-life/board.py
@@ -0,0 +1,91 @@
+from typing import List
+
+
+class Board:
+    __NO_CELL = False
+    __CELL = True
+    __DISPLAY_VALUES = {
+        __NO_CELL: '.',
+        __CELL: 'O',
+    }
+
+    __board: List[List[bool]]
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.__board = [[self.__NO_CELL for col in range(width)] for row in range(height)]
+
+    def next_generation(self) -> None:
+        next_gen = []
+
+        for row_idx, row in enumerate(self.__board):
+            next_gen.append([])
+            for col_idx, cell in enumerate(row):
+                neighbours_count = self._get_neighbours(row_idx, col_idx)
+                should_live = neighbours_count == 3 or (cell and neighbours_count == 2)
+                next_gen[-1].append(should_live)
+
+        self.__board = next_gen
+
+    def _get_neighbours(self, row: int, col: int):
+        neighbours = 0
+
+        can_go_up = row - 1 >= 0
+        can_go_down = row + 1 < self.__height
+        can_go_left = col - 1 >= 0
+        can_go_right = col + 1 < self.__width
+
+        UP = LEFT = -1
+        RIGHT = DOWN = 1
+
+        if can_go_right:
+            neighbours += int(self.__board[row][col+RIGHT])
+        if can_go_left:
+            neighbours += int(self.__board[row][col+LEFT])
+        if can_go_down:
+            neighbours += int(self.__board[row+DOWN][col])
+        if can_go_up:
+            neighbours += int(self.__board[row+UP][col])
+        if can_go_up and can_go_right:
+            neighbours += int(self.__board[row+UP][col+RIGHT])
+        if can_go_down and can_go_right:
+            neighbours += int(self.__board[row+DOWN][col+RIGHT])
+        if can_go_down and can_go_left:
+            neighbours += int(self.__board[row+DOWN][col+LEFT])
+        if can_go_up and can_go_left:
+            neighbours += int(self.__board[row+UP][col+LEFT])
+
+        return neighbours
+
+    def spawn_cell(self, row: int, col: int):
+        self.__board[row][col] = self.__CELL
+
+    def kill_cell(self, row: int, col: int):
+        self.__board[row][col] = self.__NO_CELL
+
+    @classmethod
+    def from_string(cls, string_repr: str) -> 'Board':
+        rows = string_repr.strip('\n \t').split('\n')
+        board = Board(height=len(rows), width=len(rows[0]))
+
+        for row_idx, row in enumerate(rows):
+            for col_idx, cell_repr in enumerate(row.strip()):
+                if cell_repr != cls.__DISPLAY_VALUES[cls.__NO_CELL]:
+                    board.spawn_cell(row_idx, col_idx)
+
+        return board
+
+    @classmethod
+    def from_file(cls, file_name: str) -> 'Board':
+        with open(file_name) as f:
+            lines = f.readlines()
+            cls.from_string('\n'.join(lines))
+
+    def __str__(self) -> str:
+        rows = [' '.join([self.__DISPLAY_VALUES[col] for col in row]) for row in self.__board]
+
+        return '\n'.join(rows)
+
+    def _print_row(self, row_idx):
+        print(self.__board[row_idx])
---
+++ exercises-python-oop/04-encapsulation/game-of-life/test.py
@@ -0,0 +1,5 @@
+import os
+
+print('fooo')
+os.system('clear')
+print('fooo')
